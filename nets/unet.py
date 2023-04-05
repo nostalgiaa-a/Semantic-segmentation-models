ecc446f40d327d1fac8161c8fea98464dfc380c1
Newnew <76758446+nostalgiaa-a@users.noreply.github.com>
2023-04-05 18:18:43 +0800

    Add files via upload

--- nets/unet.py
+++ nets/unet.py
@@ -1,1 +1,94 @@
+""" Full assembly of the parts to form the complete network """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class Down(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()   
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+    
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        # super().__init__()
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, n_classes):
+        super(UNet, self).__init__()
+        self.in_channels = in_channels
+        self.n_classes = n_classes
+        # self.bilinear = bilinear
+
+        # self.inc = DoubleConv(n_channels, 64)
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 #if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor)
+        self.up2 = Up(512, 256 // factor)
+        self.up3 = Up(256, 128 // factor)
+        self.up4 = Up(128, 64)
+        self.outc = OutConv(64, n_classes)
+        self.softmax = nn.Softmax()
+        # self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x1 = self.double_conv(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        logits = self.softmax(logits)
+        return logits
